f3989bde861f95605af2e877c1694e67a94fb600
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-05-22 11:04:15 -0400

    M

--- .output/public/_nuxt/hello-121f6bfb.mjs
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as r}from"./entry-f6e039dd.mjs";const t={};function c(a,n,s,l,_,p){return o(),r("div",null," Hello ")}var d=e(t,[["render",c]]);export{d as default};
--- .output/public/_nuxt/index-924ef79b.mjs
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as n,c as o}from"./entry-f6e039dd.mjs";const r={};function t(c,a,s,_,p,d){return n(),o("div",null," Index ")}var i=e(r,[["render",t]]);export{i as default};
